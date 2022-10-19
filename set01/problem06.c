736ef9ce3b51d7a7bd094aac8ea862b051129d0f
Ananya  S <114483220+ananya-s-bhat@users.noreply.github.com>
2022-10-19 07:53:56 +0000

    error

---
+++ set01/problem06.c
@@ -0,0 +1,45 @@
+# include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter the number");
+  scanf("%d",&a);
+  return a;
+  
+  
+}
+int compare(int a, int b, int c, int *largest)
+{
+  
+  if(a>b && a>c)
+  {
+    *largest=a;
+    
+  }
+  else if (b>a && b>c)
+  {
+    *largest=b;
+  }
+ else
+    *largest=c;
+   return *largest; 
+}
+void output( int a , int b ,int c, int largest)
+{
+  printf("%d is largest number ",largest);
+  
+}
+int main()
+{
+  int p,q,r,s;
+  
+  p=input();
+  q=input();
+  r=input();
+  
+  compare(p,q,r,s);
+  output(p,q,r,s);
+  return 0;
+  
+  
+}
