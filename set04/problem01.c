d4217b8a967e6854a8e88a8c0b55fa645b0c5a54
Ananya  S <114483220+ananya-s-bhat@users.noreply.github.com>
2023-01-18 17:15:47 +0000

    done

--- set04/problem01.c
+++ set04/problem01.c
@@ -6,16 +6,27 @@ void output(float x1, float y1, float x2, float y2, float distance);
 void input(float *x1, float *y1, float *x2, float *y2)
 {
   printf("enter the 1 point ");
-  scanf("%d%d",x1,y1);
+  scanf("%f%f",x1,y1);
   printf("enter the 2 point ");
-  scanf("%d%d",x2,y2);
+  scanf("%f%f",x2,y2);
   
 }
 float find_distance(float x1, float y1, float x2, float y2)
 {
   float a,b;
-  b=(((y2-y1)*(y2-y1))
+  b=((y2-y1)*(y2-y1)+(x2-x1)*(x2-x1));
   a=sqrt(b);
-  
+  return a;
 }
-void output(float x1, float y1, float x2, float y2, float distance);
+void output(float x1, float y1, float x2, float y2, float distance)
+{
+  printf("the distance between two points is%f",distance);
+}
+int main()
+{
+  float a1,a2,b1,b2,d;
+  input(&a1,&a2,&b1,&b2);
+  d=find_distance(a1,a2,b1,b2);
+  output(a1,a2,b1,b2,d);
+  return 0;
+}
